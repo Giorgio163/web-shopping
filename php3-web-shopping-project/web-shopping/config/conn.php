24e8db1ca0af7bc1b73c19bb85caa4d90d0b0930
Giorgio Selmi <selmi.giorgio@gmail.com>
2023-01-16 20:43:40 +0200

    modified configuration

--- php3-web-shopping-project/web-shopping/config/conn.php
+++ php3-web-shopping-project/web-shopping/config/conn.php
@@ -1,8 +1,11 @@
 <?php
-
+$host = 'localhost';
+$dbname = 'web_shopping';
+$username = 'root';
+$password = '';
 
 try {
-    return new PDO('mysql:host=localhost;dbname=web_shopping', 'root', '');
+    return new PDO("mysql:host=$host;dbname=$dbname", "$username", '');
 } catch (PDOException $exception) {
     error_log($exception->getMessage());
     die('Something went wrong. Please, try again later.');
--- php3-web-shopping-project/web-shopping/database/create-tables.php
+++ php3-web-shopping-project/web-shopping/database/create-tables.php
@@ -3,7 +3,10 @@
 
 $sql = file_get_contents(__DIR__ . '/tables.sql');
 
+$reCreateDatabase = readline('Do you want to re-create your database? [no] ');
 $pdo = require __DIR__ . '/../config/conn.php';
-
+if ($reCreateDatabase === 'yes') {
+    $pdo->exec('DROP DATABASE web_shopping');
+}
 $pdo->exec($sql);
 echo 'Tables created! :)';
